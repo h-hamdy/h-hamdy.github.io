4da6c70be868e2d8a1320a27aaca183da57c1a15
h-hamdy <houssamhamdy223@gmail.com>
2023-12-19 15:59:08 +0100

    Resize Blog Card

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -14,20 +14,20 @@ export function BlogCard({ link, img, time, title, desc }: Props) {
         className="hover:scale-110 transition-transform duration-300 lg:pt-10"
       >
         <div className="h-full overflow-y-auto overflow-hidden">
-          <div className="w-[300px] h-[300px] bg-[#343838] shadow-2xl custom-corner transform card-container">
-            <div className="flex items-center justify-center bg-white custom-corner w-[300px] h-[200px]">
+          <div className="lg:w-[300px] lg:h-[300px] w-[250px] h-[250px] bg-[#343838] shadow-2xl custom-corner transform card-container">
+            <div className="flex items-center justify-center bg-white custom-corner lg:w-[300px] lg:h-[200px] w-[250px] h-[150px]">
               <img
                 src={img}
-                className="w-full h-[200px] custom-corner object-fit"
+                className="w-full lg:h-[200px] h-[150px] custom-corner object-fit"
                 alt="Tree Image"
               />
             </div>
             <div className="p-4">
               <div className="text-xs text-gray-400">{time}</div>
-              <div className="text-white text-xl font-semibold tracking-wide">
+              <div className="text-white lg:text-xl text-md font-semibold tracking-wide">
                 {title}
               </div>
-              <div className="text-sm text-gray-200">{desc}</div>
+              <div className="lg:text-sm text-xs text-gray-200">{desc}</div>
             </div>
           </div>
         </div>
