632fdaf17f727aac760fb0ca00a1abfc30589885
h-hamdy <houssamhamdy223@gmail.com>
2024-10-08 09:31:06 +0100

    Update Profile

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -14,13 +14,13 @@ export function About() {
       </div>
       <div className="h-[95%] overflow-y-auto lg:pb-40">
         <div className="flex lg:px-10 px-6 text-gray-200 text-center overflow-y-auto text-sm lg:text-md lg:tracking-wider">
-          Hello! I'm Houssam Hamdy, a Full-Stack developer fully committed to my
-          software engineering studies at 1337. Driven by self-discipline and a
-          passion for tackling real-world challenges, I enjoy delving into new
-          technologies. My excitement spills over into the ever-evolving world
-          of the web3 industry, showcasing my commitment to staying at the
-          forefront. Eager to contribute my skills and creativity, I actively
-          engage in the dynamic field of software development.
+          Hello! I am Houssam Hamdy, a Software Engineer graduated from 1337.
+          Passionate about solving real-world challenges, I thrive in
+          collaborative environments and enjoy contributing to both frontend and
+          backend development in group projects. I'm highly motivated by
+          problem-solving and constantly driven to create innovative solutions
+          that address complex issues. I am always eager to apply my skills and
+          creativity to new opportunities that make a positive impact.
         </div>
         <div className="lg:p-10 p-6 px-10">
           <div className="border w-full rounded-xl border-[#343838] bg-[#343838]"></div>
@@ -44,12 +44,16 @@ export function About() {
                 className="lg:w-[70px] lg:h-[70px] w-[40px] h-[40px] rounded-lg "
               ></img>
               <div>
-                <div className="text-sm lg:text-md">1337 (42 Network), Benguerir</div>
+                <div className="text-sm lg:text-md">
+                  1337 (42 Network), Benguerir
+                </div>
                 <div className="text-[11px] text-gray-400">
                   November 2021 - 2024
                 </div>
                 <div className="lg:text-sm text-xs text-gray-300 pt-[10px]">
-				Student at 1337 School, currently at level <span className="text-white text-sm">13.74</span>, with strong skills in software development.
+                  Student at 1337 School, currently at level{" "}
+                  <span className="text-white text-sm">13.74</span>, with strong
+                  skills in software development.
                 </div>
               </div>
             </div>
@@ -59,10 +63,10 @@ export function About() {
                 className="lg:w-[70px] lg:h-[70px] w-[40px] h-[40px] rounded-lg "
               ></img>
               <div>
-                <div className="text-sm lg:text-md">UM6P - Mohammed VI Polytechnic University · Internship</div>
-                <div className="text-[11px] text-gray-400">
-                  Janvier 2024
+                <div className="text-sm lg:text-md">
+                  UM6P - Mohammed VI Polytechnic University · Internship
                 </div>
+                <div className="text-[11px] text-gray-400">Janvier 2024</div>
                 <div className="lg:text-sm text-xs text-gray-300 pt-[10px]">
                   Full-stack Developer
                 </div>
@@ -83,74 +87,75 @@ export function About() {
             Licenses & certifications
           </div>
         </div>
-		<div className="pl-10">
-
-        <div className="lg:pl-28 px-6 py-6 text-white">
-          <div className="flex gap-[15px]">
+        <div className="pl-10">
+          <div className="lg:pl-28 px-6 py-6 text-white">
             <div className="flex gap-[15px]">
-              <img
-                src="https://raw.githubusercontent.com/h-hamdy/Portfolio/main/src/assets/ibm.png"
-                className="lg:w-[60px] lg:h-[60px] w-[40px] h-[40px] rounded-lg"
-              ></img>
-            </div>
-            <div>
-              <div className="lg:text-md text-sm">Blockchain Essentials</div>
-              <div className="text-[11px] text-gray-400">IBM</div>
-              <div className="text-[11px] text-gray-400 pb-[5px]">
-                Issued on: November 5, 2022
+              <div className="flex gap-[15px]">
+                <img
+                  src="https://raw.githubusercontent.com/h-hamdy/Portfolio/main/src/assets/ibm.png"
+                  className="lg:w-[60px] lg:h-[60px] w-[40px] h-[40px] rounded-lg"
+                ></img>
               </div>
-              <a href="https://courses.cognitiveclass.ai/certificates/71250c784c0c4e9fa24ba671b4777be4">
-				<button className="flex items-center justify-center text-sm text-gray-300 border lg:w-[150px] lg:h-[30px] w-[110px] h-[25px] rounded-xl">
-                  <div className="lg:text-md text-xs">show Certificate</div>
-                </button>
-              </a>
+              <div>
+                <div className="lg:text-md text-sm">Blockchain Essentials</div>
+                <div className="text-[11px] text-gray-400">IBM</div>
+                <div className="text-[11px] text-gray-400 pb-[5px]">
+                  Issued on: November 5, 2022
+                </div>
+                <a href="https://courses.cognitiveclass.ai/certificates/71250c784c0c4e9fa24ba671b4777be4">
+                  <button className="flex items-center justify-center text-sm text-gray-300 border lg:w-[150px] lg:h-[30px] w-[110px] h-[25px] rounded-xl">
+                    <div className="lg:text-md text-xs">show Certificate</div>
+                  </button>
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="lg:pl-28 px-6 py-6 text-white">
-          <div className="flex gap-[15px]">
+          <div className="lg:pl-28 px-6 py-6 text-white">
             <div className="flex gap-[15px]">
-              <img
-                src="https://raw.githubusercontent.com/h-hamdy/Portfolio/main/src/assets/simpli.jpg"
-                className="lg:w-[60px] lg:h-[60px] w-[40px] h-[40px] rounded-lg"
-              ></img>
-            </div>
-            <div>
-              <div className="lg:text-md text-sm">Blockchain Certification Training</div>
-              <div className="text-[11px] text-gray-400">simplilearn</div>
-              <div className="text-[11px] text-gray-400 pb-[5px]">
-                Issued on: Septembre 27, 2022
+              <div className="flex gap-[15px]">
+                <img
+                  src="https://raw.githubusercontent.com/h-hamdy/Portfolio/main/src/assets/simpli.jpg"
+                  className="lg:w-[60px] lg:h-[60px] w-[40px] h-[40px] rounded-lg"
+                ></img>
               </div>
-              <a href="https://simpli-web.app.link/e/I6kzeI3IbFb">
-			  <button className="flex items-center justify-center text-sm text-gray-300 border lg:w-[150px] lg:h-[30px] w-[110px] h-[25px] rounded-xl">
-                  <div className="lg:text-md text-xs">show Certificate</div>
-                </button>
-              </a>
+              <div>
+                <div className="lg:text-md text-sm">
+                  Blockchain Certification Training
+                </div>
+                <div className="text-[11px] text-gray-400">simplilearn</div>
+                <div className="text-[11px] text-gray-400 pb-[5px]">
+                  Issued on: Septembre 27, 2022
+                </div>
+                <a href="https://simpli-web.app.link/e/I6kzeI3IbFb">
+                  <button className="flex items-center justify-center text-sm text-gray-300 border lg:w-[150px] lg:h-[30px] w-[110px] h-[25px] rounded-xl">
+                    <div className="lg:text-md text-xs">show Certificate</div>
+                  </button>
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="lg:pl-28 px-6 py-6 text-white">
-          <div className="flex gap-[15px]">
+          <div className="lg:pl-28 px-6 py-6 text-white">
             <div className="flex gap-[15px]">
-              <img
-                src="https://raw.githubusercontent.com/h-hamdy/Portfolio/main/src/assets/coursera.png"
-                className="lg:w-[60px] lg:h-[60px] w-[40px] h-[40px] rounded-lg"
-				></img>
-            </div>
-            <div className="">
-              <div className="lg:text-md text-sm">Smart Contracts</div>
-              <div className="text-[11px] text-gray-400">Coursera</div>
-              <div className="text-[11px] text-gray-400 pb-[5px]">
-                Issued on: Novembre 25, 2022
+              <div className="flex gap-[15px]">
+                <img
+                  src="https://raw.githubusercontent.com/h-hamdy/Portfolio/main/src/assets/coursera.png"
+                  className="lg:w-[60px] lg:h-[60px] w-[40px] h-[40px] rounded-lg"
+                ></img>
               </div>
-              <a href="https://www.coursera.org/account/accomplishments/verify/UVUXGWNV59VB?utm_source=link&utm_medium=certificate&utm_content=cert_image&utm_campaign=sharing_cta&utm_product=course">
-                <button className="flex items-center justify-center text-sm text-gray-300 border lg:w-[150px] lg:h-[30px] w-[110px] h-[25px] rounded-xl">
-                  <div className="lg:text-md text-xs">show Certificate</div>
-                </button>
-              </a>
+              <div className="">
+                <div className="lg:text-md text-sm">Smart Contracts</div>
+                <div className="text-[11px] text-gray-400">Coursera</div>
+                <div className="text-[11px] text-gray-400 pb-[5px]">
+                  Issued on: Novembre 25, 2022
+                </div>
+                <a href="https://www.coursera.org/account/accomplishments/verify/UVUXGWNV59VB?utm_source=link&utm_medium=certificate&utm_content=cert_image&utm_campaign=sharing_cta&utm_product=course">
+                  <button className="flex items-center justify-center text-sm text-gray-300 border lg:w-[150px] lg:h-[30px] w-[110px] h-[25px] rounded-xl">
+                    <div className="lg:text-md text-xs">show Certificate</div>
+                  </button>
+                </a>
+              </div>
             </div>
           </div>
-				</div>
         </div>
         <div></div>
       </div>
--- src/components/profile/ProfileCard.tsx
+++ src/components/profile/ProfileCard.tsx
@@ -23,8 +23,8 @@ export function ProfileCard() {
 			</div>
 
           <img
-            className="border border-[#343838] bg-[#343838] custom-corner lg:w-[120px] w-[80px] h-[80px] lg:h-[120px] shadow-xl transform transition-transform duration-300 hover:scale-110"
-            src="https://cdn.intra.42.fr/users/0d6b52298379f4824e232a37e7049fac/hhamdy.jpg"
+            className="border border-[#343838] bg-[#343838] object-cover custom-corner lg:w-[120px] w-[80px] h-[80px] lg:h-[120px] shadow-xl transform transition-transform duration-300 hover:scale-110"
+            src="src/assets/profissional.jpg"
 			/>
 		  <div className="flex flex-col items-center justify-around gap-[15px] pt-[15px] lg:pt-0">
 
